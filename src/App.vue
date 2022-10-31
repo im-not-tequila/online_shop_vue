124fe945c9ff2229f2a901b5f822b7bec3ec2343
im-not-tequila <i.am.tequila1236@gmail.com>
2022-10-31 17:44:19 +0600

    changes

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,14 @@
       <div class="navbar-brand">
         <router-link to="/" class="navbar-item"><strong>Магазин</strong></router-link>
 
-        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
 
-      <div class="navbar-menu" id="navbar-menu">
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu }">
         <div class="navbar-end">
           <router-link to="/tables" class="navbar-item">Столы</router-link>
           <router-link to="/chairs" class="navbar-item">Стулья</router-link>
@@ -19,14 +19,37 @@
           <div class="navbar-item">
             <div class="buttons">
               <router-link to="/login" class="button is-light">Вход</router-link>
+
+              <router-link to="/cart" class="button is-success">
+                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                <span>Корзина</span>
+              </router-link>
             </div>
           </div>
+
         </div>
       </div>
     </nav>
-    <router-view/>
+
+    <section class="section">
+        <router-view/>
+    </section>
+
+    <footer class="footer">
+      <p class="has-text-centered">Copyright (c) 2022</p>
+    </footer>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        showMobileMenu: false,
+      }
+    }
+  }
+</script>
 
 <style lang="scss">
 @import '../node_modules/bulma';
