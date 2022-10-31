146937902324e280c6b95580e87c1549397eb95a
im-not-tequila <i.am.tequila1236@gmail.com>
2022-10-31 16:54:28 +0600

    first

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong>Магазин</strong></router-link>
+
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="navbar-menu" id="navbar-menu">
+        <div class="navbar-end">
+          <router-link to="/tables" class="navbar-item">Столы</router-link>
+          <router-link to="/chairs" class="navbar-item">Стулья</router-link>
+
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link to="/login" class="button is-light">Вход</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <router-view/>
+  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import '../node_modules/bulma';
 </style>
